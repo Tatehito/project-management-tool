a6b2ee4830098fcbce385337fbfc0115fabf4585
Tatehito <tatehito.st@gmail.com>
2020-03-04 11:50:00 +0900

    Create scaffold Team.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_024216) do
+ActiveRecord::Schema.define(version: 2020_03_04_024616) do
 
   create_table "members", force: :cascade do |t|
     t.string "name"
     t.boolean "owner", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
