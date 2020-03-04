c2c33cfadc0dbf2ad8d646149ffaa22a268c01a6
Tatehito <tatehito.st@gmail.com>
2020-03-04 11:53:07 +0900

    Create model TeamMember.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_024616) do
+ActiveRecord::Schema.define(version: 2020_03_04_025101) do
 
   create_table "members", force: :cascade do |t|
     t.string "name"
     t.boolean "owner", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "team_members", force: :cascade do |t|
+    t.boolean "leader", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
