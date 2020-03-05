f64f65d9195dc2fae24c0aa686d0e59c7053e27b
Tatehito <tatehito.st@gmail.com>
2020-03-05 17:59:44 +0900

    自チームからメンバーを外す機能を実装

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -1,17 +1,28 @@
 class TeamMembersController < ApplicationController
+  before_action :set_team_member, only: [:destroy]
+
   def show
     @team = Team.find(params[:id])
     @new_member = @team.team_members.build
   end
 
   def create
-    team_member = TeamMember.new(team_member_params)
-    if team_member.save
-      redirect_to action: 'show', id: team_member.team_id
+    new_member = TeamMember.new(team_member_params)
+    if new_member.save
+      redirect_to action: 'show', id: new_member.team_id
     end
   end
 
+  def destroy
+    @team_member.destroy
+    redirect_to action: 'show', id: @team_member.team_id
+  end
+
   private
+
+  def set_team_member
+    @team_member = TeamMember.find(params[:id])
+  end
 
   def team_member_params
     params.require(:team_member).permit(:member_id, :team_id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root 'members#login'
   resources :teams
   resources :members
-  get '/team_members/:id', to: 'team_members#show'
-  post '/team_members/add', to: 'team_members#create'
+  resources :team_members
 end
