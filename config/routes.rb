8f17f87d3a4aec31b91db686c766e0b1d6d16180
Tatehito <tatehito.st@gmail.com>
2020-03-06 22:51:59 +0900

    Roomモデルを生成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :teams
   resources :members
   resources :team_members
+  resources :rooms
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_032413) do
+ActiveRecord::Schema.define(version: 2020_03_06_134922) do
 
   create_table "members", force: :cascade do |t|
     t.string "name"
     t.boolean "owner", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
