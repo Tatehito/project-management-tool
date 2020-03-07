794472a5a60528621cd0d8bc118e0319a7c4bb81
Tatehito <tatehito.st@gmail.com>
2020-03-07 14:53:56 +0900

    チームメンバーエディット画面が開けない不具合の修正（leader?メソッドの重複）

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,14 +5,14 @@ class Member < ApplicationRecord
   has_many :meeting_members, dependent: :destroy
   has_many :meetings,   through: :meeting_members
 
-  def leader?(team)
-    team.leader == self
-  end
-
-  def leader?
-    team_members.each do |m|
-      return true if m.leader
+  def leader?(team=nil)
+    if team.nil?
+      team_members.each do |m|
+        return true if m.leader
+      end
+      false
+    else
+      team.leader == self
     end
-    false
   end
 end
