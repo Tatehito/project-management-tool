427f8969d51b5f26c893c242c1d0e47392a9de6d
Tatehito <tatehito.st@gmail.com>
2020-03-07 15:14:01 +0900

    Meeting edit関連の画面遷移先を修正

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -36,14 +36,10 @@ class MeetingsController < ApplicationController
   # PATCH/PUT /meetings/1
   # PATCH/PUT /meetings/1.json
   def update
-    respond_to do |format|
-      if @meeting.update(meeting_params)
-        format.html { redirect_to @meeting, notice: 'Meeting was successfully updated.' }
-        format.json { render :show, status: :ok, location: @meeting }
-      else
-        format.html { render :edit }
-        format.json { render json: @meeting.errors, status: :unprocessable_entity }
-      end
+    if @meeting.update(meeting_params)
+      redirect_to edit_meeting_path, notice: 'Meeting was successfully updated.'
+    else
+      render :edit
     end
   end
 
