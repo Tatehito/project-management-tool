620d203f58475d2ecc97a684f6ca76d5b4f407ad
Tatehito <tatehito.st@gmail.com>
2020-03-09 16:44:21 +0900

    ヘッダーにログインユーザーの名前を表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user, :logged_in?
   
   private
 
   def current_user
     Member.find(session[:user_id])
+  end
+
+  def logged_in?
+    !session[:user_id].nil?
   end
 end
