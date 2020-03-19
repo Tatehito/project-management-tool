9ab7341cad10e04eae2089b334d59046f347358a
Tatehito <tatehito.st@gmail.com>
2020-03-19 12:26:54 +0900

    予約済みの会議室でミーティングを開催できないようvalidation追加

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -6,10 +6,21 @@ class Meeting < ApplicationRecord
   has_many :members,   through: :meeting_members
 
   validate :date_reversal
+  validate :reserved
 
   def date_reversal
     if start_time > end_time
       errors.add(:base, '開始時間と終了時間が逆転しています。')
+    end
+  end
+
+  def reserved
+    room.meetings.each do |m|
+      if (start_time <= m.start_time && end_time >= m.start_time) ||
+         (start_time >= m.start_time && start_time <= m.end_time)
+        errors.add(:base, "会議室「#{room.name}」は予約済みです。時間を変更してください。")
+        break
+      end
     end
   end
 
