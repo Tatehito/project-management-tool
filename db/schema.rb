dd9f937463fb318f3c574b04b3210b028483a2d4
Tatehito <tatehito.st@gmail.com>
2020-03-24 12:36:31 +0900

    bookモデルを生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_081634) do
+ActiveRecord::Schema.define(version: 2020_03_24_033515) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "meeting_members", force: :cascade do |t|
     t.integer "member_id", null: false
